13f5dfff3660581026a6beb1f4d7fc1d5978e8b5
Charles-henry DESVERNAY <cdesvern@e2r8p12.42.fr>
2016-11-01 16:42:19 +0100

    created parser

--- minishell/parser101/cmd_line_parser.c
+++ minishell/parser101/cmd_line_parser.c
@@ -6,46 +6,119 @@
 /*   By: cdesvern <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/27 17:29:18 by cdesvern          #+#    #+#             */
-/*   Updated: 2016/10/27 18:10:56 by cdesvern         ###   ########.fr       */
+/*   Updated: 2016/11/01 16:29:22 by cdesvern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "cmd_line_parser.h"
 
-staticic char	*clp_getline(int fd, char *buff)
+static int	clp_ignore(int fd, char *buff, char **out, int i)
 {
-	int		i;
+	int		ret;
+	char	*cp;
 
-	i = 0;
-	while (buff[i])
+	if (buff[i + 1] == 0)
+	{
+		cp = *out;
+		*out = ft_strjoin(*out, buff);
+		if (cp)
+			free(cp);
+		ret = read(fd, buff, CLP_BUFFSIZE);
+		buff[ret] = 0;
+		return (1);
+	}
+	else
+		return (i + 2);
+}
+
+static int	clp_nextquote(int fd, char *buff, char **out, int i)
+{
+	int		ret;
+	char	*cp;
+	char	delim;
+
+	delim = buff[i++];
+	while (buff[i] != delim)
 	{
 		if (buff[i] == '\\')
-			i += clp_ignore(fd, buff);
-		else if (buff[i] == '"')
-			i += clp_nextquote(fd, buff);
-		else if (buff[i] == '\n')
-			return (clp_buildline(buff));
+			i = clp_ignore(fd, buff, out, i);
+		else if (buff[i] == 0)
+		{
+			cp = *out;
+			*out = ft_strjoin(*out, buff);
+			if (cp)
+				free(cp);
+			ret = read(fd, buff, CLP_BUFFSIZE);
+			buff[ret] = 0;
+			i = 0;
+		}
 		else
 			i++;
 	}
+	return (i + 1);
+}
+
+static char	*clp_buildline(int fd, char *buff, char	**out, int i)
+{
+	char	*cp;
+
+	buff[i] = 0;
+	cp = *out;
+	*out = ft_strjoin(*out, buff);
+	if (cp)
+		free(cp);
+	ft_memmove(buff, buff + i + 1, CLP_BUFFSIZE - i - 1);
+	return (*out);
+}
+
+static char	*clp_getline(int fd, char *buff, char **out)
+{
+	char	*cp;
+	int		i;
+	int		ret;
+
+	i = 0;
+	while (buff[i] != '\n')
+	{
+		if (buff[i] == '\\')
+			i = clp_ignore(fd, buff, out, i);
+		else if (buff[i] == '"')
+			i = clp_nextquote(fd, buff, out, i);
+		else if (buff[i] == 0)
+		{
+			cp = *out;
+			*out = ft_strjoin(*out, buff);
+			if (cp)
+				free(cp);
+			read(fd, buff, CLP_BUFFSIZE);
+			i = 0;
+		}
+		else
+			i++;
+	}
+	clp_buildline(fd, buff, out, i);
+	return (*out);
 }
 
 int		ft_cmd_parser(int fd, char **line)
 {
 	static char	buff[CLP_BUFFSIZE + 1];
 	int			ret;
-	char		*out;
+	char		**out;
 
 	if (!line || fd < 0)
 		return (-1);
 	if (!*buff)
 	{
 		ret = read(fd, buff, CLP_BUFFSIZE);
-		if (ret = 0)
+		if (ret == 0)
 			return (-1);
 		buff[ret] = 0;
 	}
-	out = clp_getline(fd, buff);
-	*line = out;
+	if (!(out = ft_memalloc(sizeof(char*))))
+		exit(-1);
+	*line = clp_getline(fd, buff, out);
+	free(out);
 	return (1);
 }
